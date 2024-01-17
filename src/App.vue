1933d9c448eb2df1ea0dc9c6d62175dd620f0e4e
Richiisontheway <mineiriccardo@gmail.com>
2024-01-17 15:34:57 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+// importazione del file
+import Header from './components/Header.vue'
+// my-component.js
+export default {
+  data() {
+    return 
+  },
+  // aggiungo il nome del file come componente
+  components: {
+    Header
+  }
+}
+</script>
+
+<template>
+      <!-- Aggiungo il tag con il nome del componente all'interno
+        del tag template -->
+      <Header />
+
+      
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+data() {
+    return {
+        
+    }
+},
+
+}
+</script>
+<template>
+
+    <div>
+        <h1>
+            
+        </h1>
+    </div>
+
+</template>
